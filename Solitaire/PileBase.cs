d7ee8dedc94ac53a553db799e9af2519e09c87ea
arou1990 <arou1990@gmail.com>
2023-04-24 23:35:23 -0400

    Initial Commit

---
+++ Solitaire/PileBase.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solitaire;
+public class PileBase
+{
+    public string Location { get; set; }
+    public List<Card> Cards { get; set; } = new List<Card>();
+
+    public PileBase(string location)
+    {
+        Location = location;
+    }
+
+
+
+    public int Count
+    {
+        get
+        {
+            return Cards.Count;
+        }
+    }
+
+    public void Add(Card card)
+    {
+        if (card != null)
+        {
+            card.Location = Location;
+            Cards.Add(card);
+        }
+    }
+
+    public bool Any()
+    {
+        return Cards.Any();
+    }
+
+    public Card Pop()
+    {
+        var lastCard = Cards.LastOrDefault();
+        if (lastCard != null)
+        {
+            Cards.Remove(lastCard);
+        }
+
+        return lastCard;
+    }
+
+    public Card Last()
+    {
+        return Cards.LastOrDefault();
+    }
+
+    public void RemoveIfExists(Card card)
+    {
+        var matchingCard = Cards.FirstOrDefault(x => x.Suit == card.Suit && x.Value == card.Value);
+        if (matchingCard != null)
+            Cards.Remove(matchingCard);
+    }
+
+    public bool Contains(Card card)
+    {
+        return Cards.Any(x => x.Suit == card.Suit && x.Value == card.Value);
+    }
+
+    public void PrintCard()
+    {
+        Console.Write(Cards[Cards.Count - 1].DisplayName);
+    }
+
+    public void PrintPile()
+    {
+        foreach(Card card in Cards)
+        {
+            if (card.IsVisible)
+            {
+                Console.WriteLine(card.DisplayName);
+            }
+            else
+            {
+                Console.WriteLine("[]");
+            }
+        }
+    }
+
+    public void PrintEmptyPile()
+    {
+        if(Cards.Count == 0)
+        {
+            Console.Write("+");
+        }
+    }
+
+    
+}
---
+++ Solitaire/StackPile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solitaire;
+
+    public class StackPile : PileBase
+    {
+    public StackPile(string location) : base(location)
+    {
+    }
+
+    public int IndexOf(Card card)
+        {
+            var matchingCard = Cards.FirstOrDefault(x => x.Suit == card.Suit && x.Value == card.Value);
+            if (matchingCard != null)
+                return Cards.IndexOf(matchingCard);
+
+            return 0;
+        }
+
+        public bool HasNoHiddenCards()
+        {
+            return !Any() || Cards.All(x => x.IsVisible);
+        }
+
+        public int Count()
+        {
+            return Cards.Count();
+        }
+
+        public List<Card> GetAllCards()
+        {
+            return Cards;
+        }
+    }
